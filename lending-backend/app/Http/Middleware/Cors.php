4ecf503e044d3ce14f1553fa25981e6b929b8585
Jeamko23590 <nositerajearemyniko21@gmail.com>
2025-12-27 09:09:59 +0800

    Fix CORS middleware

--- lending-backend/app/Http/Middleware/Cors.php
+++ lending-backend/app/Http/Middleware/Cors.php
@@ -4,25 +4,30 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class Cors
 {
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next): Response
     {
         // Handle preflight OPTIONS request
         if ($request->isMethod('OPTIONS')) {
-            return response('', 200)
-                ->header('Access-Control-Allow-Origin', '*')
-                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin')
-                ->header('Access-Control-Max-Age', '86400');
+            $response = response('', 200);
+            $this->addCorsHeaders($response);
+            return $response;
         }
 
         $response = $next($request);
+        $this->addCorsHeaders($response);
+        
+        return $response;
+    }
 
-        return $response
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin');
+    private function addCorsHeaders($response): void
+    {
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin');
+        $response->headers->set('Access-Control-Max-Age', '86400');
     }
 }
